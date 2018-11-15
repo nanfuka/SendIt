3f7e82926c5c31701e1db5f5bb25be7ffcf670d6
nanfuka <kalungi2k6@gmail.com>
2018-11-15 16:10:16 +0300

    Refactor code

--- UI/js/general.js
+++ UI/js/general.js
@@ -2,7 +2,23 @@ function Destination() {
 
     var name = prompt("Please enter your new destination");
     if (name != null) {
-        document.getElementById("des").innerHTML = name
+        document.getElementById("destination").innerHTML = name
+    }
+}
+
+function Location() {
+
+    var name = prompt("Please enter the parcels current location");
+    if (name != null) {
+        document.getElementById("current").innerHTML = name
+    }
+}
+
+function Status() {
+
+    var name = prompt("Please enter the parcels current status");
+    if (name != null) {
+        document.getElementById("status").innerHTML = name
     }
 }
 
@@ -10,59 +26,7 @@ function deletes() {
     var txt;
     if (confirm("DO YOU REALLY WANT TO DELETE THIS ORDER")) {
         txt = "ORDER DELETED";
-
-    } else {
-        txt = "You pressed Cancel!";
-    }
-
-    function dis() {
-
-    }
-    document.getElementById("demo").innerHTML = txt;
-}
-
-document.getElementById("date").innerHTML = Date();
-
-    function myFunction() {
-
-        var use = document.getElementById("username");
-        document.getElementById("loc").innerHTML = use.value;
-
-                var ko = document.getElementById("sil");
-                document.getElementById("fly").innerHTML = ko.value;
-
-            }
-
-        function Location() {
-            var x;
-            var name = prompt("Please enter the parcel's current location");
-            if (name != null) {
-                document.getElementById("current").innerHTML = name
-            }
-        }
-
-        function Status() {
-            var x;
-            var name = prompt("Please enter the parcel's status");
-            if (name != null) {
-                document.getElementById("status").innerHTML = name
-            }
-        }
-
-        function deletes() {
-            var txt;
-            if (confirm("DO YOU REALLY WANT TO DELETE THIS ORDER")) {
-                txt = "ORDER DELETED";
-
-            } else {
-                txt = "You pressed Cancel!";
-            }
-
-            function dis() {
-
-            }
-            document.getElementById("demo").innerHTML = txt;
-        }
-   
-
+        txts = document.getElementById("demo").innerHTML;
         
+    }
+}
