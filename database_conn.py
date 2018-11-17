cb60da3d0be06427cae16351044610bf88a7c8ec
nanfuka <kalungi2k6@gmail.com>
2018-11-17 21:04:22 +0300

    Create sendit database and tables [Finishes #162031019]

---
+++ database_conn.py
@@ -0,0 +1,49 @@
+import psycopg2
+
+
+
+class Database:
+    def __init__(self):
+        try:
+            postgresdb = 'sendit'
+            Host="localhost"
+            User="postgres"
+            Password="test"
+
+            
+            self.connection = psycopg2.connect(
+                    database=postgresdb, host=Host, user=User,
+                    password=Password, port="5432"
+                )
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+        except:
+            print('cannot connect to database')
+
+    def create_table_users(self):
+        create_table_users = """CREATE TABLE IF NOT EXISTS users(
+            user_id SERIAL PRIMARY KEY,
+            username VARCHAR(100),
+            email VARCHAR(100),
+            password VARCHAR(100))"""
+        self.cursor.execute(create_table_users)
+        self.connection.commit()
+
+    def create_table_parsels(self):
+        create_table_parsels = """CREATE TABLE IF NOT EXISTS parcels(
+            parcel_id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            name_of_reciever VARCHAR(100) NOT NULL,
+            source VARCHAR(100) NOT NULL,
+            destination VARCHAR(100) NOT NULL,
+            status VARCHAR DEFAULT 'in_transit',
+            FOREIGN KEY (user_id)
+                REFERENCES users(user_id)
+        )"""
+        self.cursor.execute(create_table_parsels)
+        self.connection.commit()
+
+database = Database()
+database.create_table_users()
+database.create_table_parsels()
+
